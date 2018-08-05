626cba5743ae54dbbb4ab57054bf4780ea4c8c62
krishnaw14 <krishnaw14@gmail.com>
2018-08-06 03:59:15 +0530

    Organised myHM.m for submission

--- assignment1/Q2/codes/myHM.m
+++ assignment1/Q2/codes/myHM.m
@@ -1,33 +1,22 @@
 function output= myHM(input_path, ref_path)
 
 image = im2double(imread(input_path));
-%figure, imshow(image);
 ref = im2double(imread(ref_path));
-%figure, imshow(ref);
 
 M = size(image,1);
 N = size(image,2);
 C = size(image,3);
 
-% fprintf('M = %f \n', M);
-% fprintf('N = %f \n', N);
-% fprintf('C = %f \n', C);
-
-%image_cdf = get_CDF(image);
-%ref_cdf = get_CDF(image);
-
 output = uint8(ones(size(image)));
 
 for c = 1:C
-    image_cdf = get_CDF(image(:,:,c));
+    image_cdf = get_CDF(image(:,:,c)); %get_CDF defined below computed CDF of a single channel image
     ref_cdf = get_CDF(ref(:,:,c));
     for i=1:M
         for j = 1:N
             index = uint8(image(i,j,c)*255);
             image_cdf_value = image_cdf(index+1);
-            %fprintf("IMAGE CDF VALUE!!! %f\n",image_cdf_value);
-            %output(i,j,c) = find((ref_cdf-image_cdf_value)*1 >= 0.00001  , 1);
-            [value, inverse] = min( abs(ref_cdf-image_cdf_value) );
+            [~, inverse] = min( abs(ref_cdf-image_cdf_value) ); % Index of the value closest to index is essentially the inverse of the CDF function
             output(i,j,c) = uint8(inverse);
         end
     end
@@ -37,8 +26,9 @@ end
 myNumOfColors = 200;
 myColorScale = [ [0:1/(myNumOfColors-1):1]' , [0:1/(myNumOfColors-1):1]' , [0:1/(myNumOfColors-1):1]' ];
 
+%figure('name', 'Original Image')
 subplot(3,1,1)
-imshow(image, []);
+imagesc(image);
 daspect ([1 1 1]);
 axis tight;
 colormap (myColorScale);
@@ -49,7 +39,8 @@ title('Original Image')
 
 
 subplot(3,1,2)
-imshow(im2double(output), []);
+%figure('name', 'Histogram Matched Image')
+imagesc(im2double(output));
 daspect ([1 1 1]);
 axis tight;
 colormap (myColorScale);
@@ -58,18 +49,18 @@ colorbar
 title('Histogram Matched Image')
 
 subplot(3,1,3)
-imshow(histeq(image), []);
+%figure('name', 'Histogram Equalized Image')
+imagesc(histeq(image));
 daspect ([1 1 1]);
 axis tight;
 colormap (myColorScale);
 colormap jet;
 colorbar
-title('Histogram Matched Image')
+title('Histogram Equalized Image')
         
 end
 
 function CDF = get_CDF(image)
-%B = im2double(image);
 m=size(image,1);
 n=size(image,2);
 hist=imhist(image);
@@ -77,9 +68,6 @@ hist=hist/(m*n);
 CDF = zeros(size(hist));
 CDF(1)=hist(1);
 for a=2:size(hist,1)
-    %disp(i);
     CDF(a)=CDF(a-1)+hist(a);
 end
 end
-
-
