21808413bb6faf7bc0148fce6e234e6fdd226c0f
krishnaw14 <krishnaw14@gmail.com>
2018-08-06 05:47:30 +0530

    Q2 main

--- assignment1/Q2/codes/myMainScript.m
+++ assignment1/Q2/codes/myMainScript.m
@@ -3,4 +3,16 @@
 tic;
 %% Your code here
 
+
+
+input_path = '../data/barbara.png';
+HM_input_path = '../data/HMInputImage.png';
+HM_ref_path = '../data/HMRefImage.png';
+
+output_Linear = myLinearContrastStretching(input_path);
+output_HE = myHE(input_path);
+output_AHE = myAHE(input_path);
+output_CLAHE = myCLAHE(input_path);
+output_HM = myHM(HM_input_path, HM_ref_path );
+
 toc;
