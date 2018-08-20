2d082af6c86ec6dcbe88dc382e472aa4d3786858
krishnaw14 <krishnaw14@gmail.com>
2018-08-20 23:50:40 +0530

    Conflicts

--- assignmentFiltering/3/code/myMainScript.m
+++ assignmentFiltering/3/code/myMainScript.m
@@ -1,14 +1,74 @@
-%% Report - Unsharp Masking
+%% Report - Patch based filtering
 
 tic;
-%% Mainscript Code
-
-input_path = '../data/grassNoisy.mat';
-
-output = myPatchBasedFiltering(imgCorrupt);
-%imshow(output);
+%% Mainscript Code - 
 %% The parameters were chosen as follows: 
-%%% Standard deviation of the gaussian function was taken as 10
-%%% Scaling factor was taken as 1
+% Standard deviation = 10.5
+% Note that for the give values of window size and oatch size, image blurring was
+% was observed.
+
+
+input_path = '../data/grass.png';
+image1 = im2double(imread(input_path));
+image1 = imgaussfilt(image1, 0.66);
+image1 = image1(1:2:size(image1,1), 1:2:size(image1,2));
+
+output = myPatchBasedFiltering(image1, 10.5);
+
+difference = output - image1;
+RMSD = sqrt(sum(difference(:).^2))/(size(output,1)*size(output,2));
+fprintf('RMSD = %f \n', RMSD);
+%imshow(output);
+
+input_path = '../data/honeyCombReal.png';
+image2 = im2double(imread(input_path));
+image2 = imgaussfilt(image2, 0.66);
+image2 = image2(1:2:size(image2,1), 1:2:size(image2,2));
+output = myPatchBasedFiltering(image2, 10.5);
+difference = output - image2;
+RMSD = sqrt(sum(difference(:).^2))/(size(output,1)*size(output,2));
+fprintf('RMSD = %f \n', RMSD);
+
+load('../data/barbara.mat')
+image3 = imageOrig/100.0;
+image3 = imgaussfilt(image3, 0.66);
+image3 = image3(1:2:size(image3,1), 1:2:size(image3,2));
+output = myPatchBasedFiltering(image3, 10.5);
+difference = output - image3;
+RMSD = sqrt(sum(difference(:).^2))/(size(output,1)*size(output,2));
+fprintf('RMSD = %f \n', RMSD);
+
+%% For sigma = 10.5*1.1 = 11.55
+output = myPatchBasedFiltering(image3,11.55 );
+difference = output - image3;
+RMSD = sqrt(sum(difference(:).^2))/(size(output,1)*size(output,2));
+fprintf('RMSD = %f \n', RMSD);
+
+output = myPatchBasedFiltering(image3, 11.55);
+difference = output - image3;
+RMSD = sqrt(sum(difference(:).^2))/(size(output,1)*size(output,2));
+fprintf('RMSD = %f \n', RMSD);
+
+output = myPatchBasedFiltering(image3, 11.55);
+difference = output - image3;
+RMSD = sqrt(sum(difference(:).^2))/(size(output,1)*size(output,2));
+fprintf('RMSD = %f \n', RMSD);
+
+%% For sigma = 10.5*1.1 = 9.45
+output = myPatchBasedFiltering(image3,9.45 );
+difference = output - image3;
+RMSD = sqrt(sum(difference(:).^2))/(size(output,1)*size(output,2));
+fprintf('RMSD = %f \n', RMSD);
+
+output = myPatchBasedFiltering(image3, 9.45);
+difference = output - image3;
+RMSD = sqrt(sum(difference(:).^2))/(size(output,1)*size(output,2));
+fprintf('RMSD = %f \n', RMSD);
+
+output = myPatchBasedFiltering(image3, 9.45);
+difference = output - image3;
+RMSD = sqrt(sum(difference(:).^2))/(size(output,1)*size(output,2));
+fprintf('RMSD = %f \n', RMSD);
+
 
 toc;
--- assignmentFiltering/3/code/myPatchBasedFiltering.m
+++ assignmentFiltering/3/code/myPatchBasedFiltering.m
@@ -1,17 +1,24 @@
-function output = myPatchBasedFiltering(image)
+function output = myPatchBasedFiltering(image, sigma)
 
-input = image;
-imshow(input);
-corrupted_input = input + 0.05*randn(size(input));
+%input = image;
+%imshow(input);
+
+
+sd = 0.05*(max(max(image)) - min(min(image)));
+corrupted_image = image + sd*randn(size(image));
+
+input = corrupted_image;
 
 M = size(input,1);
 N = size(input,2);
 output = zeros(size(input));
 
-sigma = 4;
+%sigma = 10.5;
 
 patch_w = 4;
 size_w = 12;
+
+mask = zeros(25,25);
 
 for i = 1:M
     for j = 1:N
@@ -45,19 +52,50 @@ for i = 1:M
                 Yi = min(size(patch,2), size(patch_P,2));
                 patch_diff_matrix = patch(1:Xi, 1:Yi) - patch_P(1:Xi, 1:Yi);
                 
-                patch_diff_norm = sum( patch_diff_matrix(:).^2 ) / (Xi*Yi);
+                patch_diff_norm = sum( patch_diff_matrix(:) ) / (Xi*Yi);
                 %fprintf('W-Y : %i, P-Y = %i,  \n', size(patch_W,2),size(patch_P,2));
                 W_P(k,l) = patch_diff_norm;
             end
         end
         
-        gaussian_W_P = exp( -W_P/(sigma*sigma) );
+        gaussian_W_P = exp( -W_P.^2/(sigma*sigma) );
         weighted_avg = times(gaussian_W_P,window)/(sum(gaussian_W_P(:) ));
+        mask = weighted_avg;
         output(i,j) = sum(weighted_avg(:));
         
     end
 end
 
-imshow(mat2gray(output));
+% Displaying the input and output image 
+myNumOfColors = 200;
+myColorScale = [ [0:1/(myNumOfColors-1):1]' , [0:1/(myNumOfColors-1):1]' , [0:1/(myNumOfColors-1):1]' ];
+
+figure('name', 'Patch Based Filtering')
+subplot(1,3,1)
+imagesc(image);
+colormap (myColorScale);
+colormap gray;
+daspect ([1 1 1]);
+axis tight;
+colorbar
+title('Input Image after subsampling')
+
+subplot(1,3,2)
+imagesc(corrupted_image);
+colormap (myColorScale);
+colormap gray;
+daspect ([1 1 1]);
+axis tight;
+colorbar
+title('Corrupted Image')
+
+subplot(1,3,3)
+imagesc(output);
+colormap (myColorScale);
+colormap gray;
+daspect ([1 1 1]);
+axis tight;
+colorbar
+title('Output Image')
 
 end
