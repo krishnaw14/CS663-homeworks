33c9d2a3fb991663b8100d769fe26ec76ae1f0f2
krishnaw14 <krishnaw14@gmail.com>
2018-10-15 01:00:33 +0530

    Remove 2 for loops and store Vk

--- assignmentPCA+SVD/1/att_faces/myMainScript.m
+++ assignmentPCA+SVD/1/att_faces/myMainScript.m
@@ -1,14 +1,4 @@
 sum = zeros(112*92,1);
-for j=1:32
-    for i=1:6
-        image = im2double(imread(char(strcat('s' ,string(j) ,'/' ,string(i), '.pgm'))));
-        image = reshape(image, 112*92, 1);
-        sum = sum + image;
-    end
-end
-mean = sum/(6*32);
-
-%constructing the X matrix
 X = zeros(112*92,32*6);
 imageCount = 1;
 for j=1:32
@@ -16,27 +6,40 @@ for j=1:32
         image = im2double(imread(char(strcat('s' ,string(j) ,'/' ,string(i), '.pgm'))));
         image = reshape(image, 112*92, 1);
         imageBar = image - mean;
-        X(:,imageCount) = imageBar;
+        X(:, imageCount) = imageBar;
+        sum = sum + image;
         imageCount = imageCount + 1;
     end
 end
+mean = sum/(6*32);
+X = X - mean;
 
 L = mtimes(transpose(X), X);
-[V, D] = eig(L);%V is the matrix of eigen vectors of L; and D is the matrix of eigen values of L
+[W, D] = eig(L);%W is the matrix of eigen vectors of L; and D is the matrix of eigen values of L
 [d,ind] = sort(diag(D)); %d is the vector of eigen values in ascending order
-Vsorted = V(:,ind); %eigen vectors are arranged in ascending order of eigen values
+Wsorted = W(:,ind); %eigen vectors are arranged in ascending order of eigen values
 
-V = mtimes(X, Vsorted); %V is now the matrix of eigen vectors of the covariance matrix as required in the PCA algorithm
+
+V = mtimes(X, Wsorted); %V is now the matrix of eigen vectors of the covariance matrix as required in the PCA algorithm
 %V is the eigen space
 %unit-normalising columns of V
 for i=1:192
     V(:,i) = V(:,i) / norm(V(:,i));
 end
 
-rate = recognitionRate(1,8,1,mean,V);
+k = 20;
+
+Vk = V(:,(192-k+1):192);
+disp(size(Vk));
+disp(size(V));
+
+databaseEigenCoeffMat = dbEigCoeffMat(k, Vk, mean);
+rate = recognitionRate(1,8,k,mean,Vk);
+
+
 
 %constructing first k eigen-coefficients for each database image(training set)
-function databaseEigenCoeffMat = dbEigCoeffMat(k, V, mean)
+function databaseEigenCoeffMat = dbEigCoeffMat(k, Vk, mean)
     databaseEigenCoeffMat = zeros(k, 6*32); % first six columns for the first person, next six columns for the next person and so on...
     imageCount = 1;
     for j=1:32
@@ -44,17 +47,18 @@ function databaseEigenCoeffMat = dbEigCoeffMat(k, V, mean)
             image = im2double(imread(char(strcat('s' ,string(j) ,'/' ,string(i), '.pgm'))));
             image = reshape(image, 112*92, 1);
             imageBar = image - mean;
-            databaseEigenCoeffMat(:,imageCount) = mtimes(transpose(V(:,(192-k+1):192)) , imageBar);
+            %qp = V(:,(192-k+1):192);
+            databaseEigenCoeffMat(:,imageCount) = mtimes(transpose(Vk) , imageBar);
             imageCount = imageCount + 1;
         end
     end
 end
 
-function rate = recognitionRate(personIndex, imageIndex, k, mean, V)
-    databaseEigenCoeffMat = dbEigCoeffMat(k, V, mean);
+function rate = recognitionRate(personIndex, imageIndex, k, mean, Vk)
+    databaseEigenCoeffMat = dbEigCoeffMat(k, Vk, mean);
     testImagePath = char(strcat('s' ,string(personIndex) ,'/' ,string(imageIndex), '.pgm'));
     testImage = reshape(im2double(imread(testImagePath)), 112*92, 1);
-    testEigenCoeff = mtimes( transpose( V( :, (192-k+1):192 ) ), testImage - mean);
+    testEigenCoeff = mtimes( transpose(Vk), testImage - mean);
     dummyTestEigCoeffMat = mtimes(testEigenCoeff, ones(1,192)); 
     %dummyTestEigCoeffMat is a matrix whose each column is a eigen
     %coefficient vector
