db6e29b83cb56ebb992cf2be2312c8a542a9e73a
Krishna Wadhwani <krishnaw14@gmail.com>
2018-10-15 11:54:38 +0530

    Update yale.m

--- assignmentPCA+SVD/1/yale.m
+++ assignmentPCA+SVD/1/yale.m
@@ -1,6 +1,8 @@
 sum = zeros(168*192,1);
 X = zeros(168*192,38*40);
-imageCount = 1;
+Xtest = zeros(168*192,38*20);
+XimageCount = 1;
+Xtestimagecount = 1;
 base_path = 'data/CroppedYale/yaleB0';
 for j=1:39
     if j == 14
@@ -8,78 +10,87 @@ for j=1:39
     end
     if j > 9
         base_path = 'data/CroppedYale/yaleB';
-    end;
+    end
     path = strcat(base_path, string(j));
     image_files = dir( fullfile(path, '*.pgm') );
-    nFiles = 40;
-    disp(j);
-    disp(path)
+    nFiles = 40+20; %20 images as test set for Xtest
+    
       for i = 1:nFiles
           currentFile = fullfile(path, image_files(i).name);
-          
           image_path = convertStringsToChars(currentFile);
           image = im2double(imread(image_path));
           image = reshape(image, 168*192,1);
-          sum = sum + image;
-          X(:,imageCount) = image;
-          imageCount = imageCount + 1;
-      end
-    
+          if i<41
+            sum = sum + image;
+            X(:,XimageCount) = image;
+            XimageCount = XimageCount + 1;
+          else
+            Xtest(:,Xtestimagecount) = image;
+            Xtestimagecount = Xtestimagecount + 1;
+          end
+      end   
 end
 mean = sum/(38*40);
-imageBar = X - mean;
+X = X - mean;
+Xtest = Xtest - mean;
 
-L = mtimes(transpose(X), X);
-[W, D] = eig(L);%W is the matrix of eigen vectors of L; and D is the matrix of eigen values of L
-[d,ind] = sort(diag(D)); %d is the vector of eigen values in ascending order
-Wsorted = W(:,ind); %eigen vectors are arranged in ascending order of eigen values
+[U S V] = svd(double(X),'econ');
+% disp(size(U));
 
+k_values = [1; 2; 3; 5; 10; 15; 20; 30; 50; 60; 65; 75; 100; 200; 300; 500; 1000];
+accuracy = zeros(size(k_values));
 
-V = mtimes(X, Wsorted); %V is now the matrix of eigen vectors of the covariance matrix as required in the PCA algorithm
-%V is the eigen space
-%unit-normalising columns of V
-for i=1:192
-    V(:,i) = V(:,i) / norm(V(:,i));
+for i = 1:size(k_values)
+    k = k_values(i);
+    Vk = U(:,k);
+    databaseEigenCoeffMat = dbEigCoeffMat(k, Vk, X);
+%     fprintf('k = %i \n', k);
+    score = 0;
+    for testIndex = 1:38*20
+            personIndex = 1+ fix((testIndex-1)/20);
+%             disp(personIndex);
+            testImage = Xtest(:, testIndex);
+            [rate,index] = recognitionRate(testImage, databaseEigenCoeffMat, Vk);
+            prediction = 1+ fix((index-1)/40);
+%             if personIndex ==1
+%                 fprintf('Index = %i \n', index);
+%             end
+            if prediction == personIndex
+                score = score + 1;
+            end
+    end
+%     disp(score)
+    accuracy(i) = score;
 end
 
-k = 20;
+accuracy = accuracy/(20*38);
+[max_accuracy, best_k] = max(accuracy);
+fprintf('\n Best Accuracy on Test Set: %f percent \n', max_accuracy*100);
+fprintf('\n Best k-value: %i \n', k_values(best_k));
 
-Vk = V(:,(192-k+1):192);
-disp(size(Vk));
-disp(size(V));
-
-databaseEigenCoeffMat = dbEigCoeffMat(k, Vk, mean);
-rate = recognitionRate(1,8,k,mean,V);
-
+plot(k_values, accuracy, 'LineWidth',3);
+title('Recognition Accuracy vs. k');
+xlabel('k');
+ylabel('Test Set Accuracy');
 
 
 %constructing first k eigen-coefficients for each database image(training set)
-function databaseEigenCoeffMat = dbEigCoeffMat(k, Vk, mean)
-    databaseEigenCoeffMat = zeros(k, 6*32); % first six columns for the first person, next six columns for the next person and so on...
-    imageCount = 1;
-    for j=1:32
-        for i=1:6
-            image = im2double(imread(char(strcat('s' ,string(j) ,'/' ,string(i), '.pgm'))));
-            image = reshape(image, 112*92, 1);
-            imageBar = image - mean;
-            databaseEigenCoeffMat(:,imageCount) = mtimes(transpose(Vk) , imageBar);
-            imageCount = imageCount + 1;
-        end
+function databaseEigenCoeffMat = dbEigCoeffMat(k, Vk, X)
+    databaseEigenCoeffMat = zeros(k, 38*40); % first 40 columns for the first person, next 40 columns for the next person and so on...
+    for j=1:38*40
+            imageBar = X(:,j);
+            %qp = V(:,(192-k+1):192);
+            databaseEigenCoeffMat(:,j) = mtimes(transpose(Vk) , imageBar);
     end
 end
 
-function rate = recognitionRate(personIndex, imageIndex, k, mean, V)
-    databaseEigenCoeffMat = dbEigCoeffMat(k, V, mean);
-    testImagePath = char(strcat('s' ,string(personIndex) ,'/' ,string(imageIndex), '.pgm'));
-    testImage = reshape(im2double(imread(testImagePath)), 112*92, 1);
-    testEigenCoeff = mtimes( transpose( Vk ), testImage - mean);
-    dummyTestEigCoeffMat = mtimes(testEigenCoeff, ones(1,192)); 
-    %dummyTestEigCoeffMat is a matrix whose each column is a eigen
-    %coefficient vector
-    eigenCoeffDifferenceMat = dummyTestEigCoeffMat - databaseEigenCoeffMat;
-    recognitionRates = ones(1,192); %matrix of RMSD with eigen-coefficients of each training image 
-    for i=1:192
+function [rate,index] = recognitionRate(testImage, databaseEigenCoeffMat, Vk)
+    testEigenCoeff = mtimes( transpose(Vk), testImage);
+    eigenCoeffDifferenceMat = (databaseEigenCoeffMat - testEigenCoeff);
+    recognitionRates = zeros(1,1520); %matrix of RMSD with eigen-coefficients of each training image
+    for i=1:1520
         recognitionRates(i) = norm(eigenCoeffDifferenceMat(:,i), 'fro');
     end
-    rate = min(recognitionRates);
+
+    [rate, index] = min(recognitionRates);
 end
