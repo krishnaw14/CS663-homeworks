4f6a2b203283303f80d96976519ada4113ef5a6a
KushalYadav1916 <38888156+KushalYadav1916@users.noreply.github.com>
2018-10-15 13:06:30 +0530

    Add files via upload

--- assignmentPCA+SVD/test.m
+++ assignmentPCA+SVD/test.m
@@ -1,4 +1,4 @@
-A = [4 0 ;3 -5];
+A = [4 0;3 -5];
 [m,n]=size(A);
 B = A.';
 C = B*A;
@@ -12,31 +12,55 @@ D = diag(e);
 [m1,m2] = size(C);
 vi = zeros(n,1);
 ui = zeros(m,1);
-
+x2 = zeros(n,1);
+x3 = zeros(m,1);
 for i = 1:length(f)
     I = eye(m1);
     f(i);
     C1 = C-f(i)*I;
     x = null(C1);
+    if dot(x,x2)<0
+        x=-x
+    end
     vi = [vi x];
+    x2=x;
 end
 vi = vi(:,[2:end]);
-vi = vi.'
+vi = vi.';
 Ct = A*B;
 e = eig(C);
 
 e = sort(e,'descend');
 f=e;
 ui = zeros(m,1);
-for i = 1:length(f)
+for i = 1:length(f)-n+m
     I = eye(n1);
     f(i);
     C1 = (Ct)-f(i)*I;
-    x2 = null(C1);
-    ui = [ui x2];
+    x4 = null(C1);
+    if dot(x4,x3)<0
+        x4=-x4;
+    end
+    ui = [ui x4];
+    x3=x4;
 end
 ui = ui(:,[2:end]);
 Dn = D([1:m],[1:n]);
-ui
-vi.'
+U = ui;
+if m<n
+    V=vi;
+else
+    V=vi.';
+end
+
+EE = zeros(m,n);
+i=1;
+U*Dn*V
+if (U*Dn*V ~= A)
+    i;
+    U(3)=-U(3);
+    U(4)=-U(4);
+end  
+U
+V=V.'
 Dn
