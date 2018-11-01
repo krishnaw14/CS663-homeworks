59a21c639331acfee26bc48017d582d875901d0f
KushalYadav1916 <38888156+KushalYadav1916@users.noreply.github.com>
2018-11-01 09:02:36 +0530

    Add files via upload

---
+++ assignmentFourier/Q6/code/Q6.m
@@ -0,0 +1,19 @@
+%Image I - 300x300
+in = imread('I.png');
+[x,y]=size(in);
+%take a fourier transform
+H=fftshift(fft2(in));
+%define shift parameters
+x1 = -30;
+y1 = 70;
+%the shift in frequency domain
+[X,Y] = meshgrid(-150:149,-150:149);
+%the equation in the paper
+H=H.*exp(-1i*2*pi.*(X*x1+Y*y1)/300);
+IF = ifft2(ifftshift(H));
+%show the images
+figure;
+imshow(real(IF));
+figure;
+%time complexity - N^2
+imshow(in);
